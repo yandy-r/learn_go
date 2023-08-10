bb29ad1a56e7e50f39a523ad898e3f3bcb30c84a
Yandy Ramirez <yandyr@gmail.com>
2023-08-10 09:13:54 -0400

    print examples

--- algorithms/binsearch/main.go
+++ algorithms/binsearch/main.go
@@ -20,9 +20,16 @@ func binary_search(arr []int, target int) int {
 
 func main() {
 	arr := []int{1, 2, 3, 4, 5, 6}
-	target := 3
+	target := 5
 	index := binary_search(arr, target)
 	fmt.Println()
-	fmt.Printf("The target: %d is at index: %d\n", target, index)
+
+	fmt.Println("-------------------------------------")
+	if index != -1 {
+		fmt.Printf("The target: %d is at index: %d\n", target, index)
+	} else {
+		fmt.Printf("The target: %d is not in the array\n", target)
+	}
+	fmt.Println("-------------------------------------")
 	fmt.Println()
 }
