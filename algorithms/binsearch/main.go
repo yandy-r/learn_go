1d1db1d366f5aa24691b4a5e2e3d250097562012
Yandy Ramirez <yandyr@gmail.com>
2023-08-10 17:05:43 -0400

    use sample struct

--- algorithms/binsearch/main.go
+++ algorithms/binsearch/main.go
@@ -6,18 +6,44 @@ import (
 	"strconv"
 )
 
+/*
+NOTE: Struct Node is not necessary for binary search, but I wanted to use it to
+practice creating structs and methods. The struct contains three fields:
+high, low, and mid. The high and low fields are the high and low indexes of
+the array that the node represents. The mid field is the average of the high
+and low fields. The mid field is the index that is checked against the target value.
+*/
+
+// Node is a struct that represents a node in a binary search tree.
+type Node struct {
+	high int
+	low  int
+	mid  int
+}
+
+// NewNode creates a new node with the given high and low values and sets the
+// mid value to the average of the high and low values.
+func NewNode(high, low int) *Node {
+	return &Node{high: high, low: low, mid: (high + low) / 2}
+}
+
+// String returns a string representation of the node.
+func (n *Node) String() string {
+	return fmt.Sprintf("high: %d, low: %d, mid: %d", n.high, n.low, n.mid)
+}
+
 func binary_search(arr []int, target int) int {
-	low := 0
-	high := len(arr) - 1
-	for low <= high {
-		mid := (low + high) / 2
-		if arr[mid] == target {
-			return mid
-		} else if arr[mid] > target {
-			high = mid - 1
-		} else if arr[mid] < target {
-			low = mid + 1
+	item := NewNode(len(arr)-1, 0)
+
+	for item.low <= item.high {
+		if arr[item.mid] == target {
+			return item.mid
+		} else if arr[item.mid] > target {
+			item.high = item.mid - 1
+		} else if arr[item.mid] < target {
+			item.low = item.mid + 1
 		}
+		item.mid = (item.low + item.high) / 2
 	}
 	return -1
 }
