d000cca7110ea6f4ac5584d61fe9a48654214fcc
Yandy Ramirez <yandyr@gmail.com>
2023-08-14 22:10:20 -0400

    create placeholder functions

---
+++ projects/todo/cmd/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/kidinstinct/learn_go/projects/todo"
+)
+
+func main() {
+	todo.Placeholder()
+}
---
+++ projects/todo/todo.go
@@ -0,0 +1,9 @@
+/*
+Package todo is a simple todo list manager.
+It is a command line tool that allows you to add, remove, and list todo items.
+items are stored in a sqlite database package todo in XDG_DATA_HOME/todo/todo.db
+*/
+package todo
+
+// Placeholder is a placeholder function.
+func Placeholder()
